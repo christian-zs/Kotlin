babdeeb98de698e71a633895539fc8d2e1af0644
张森 <christian_zs@163.com>
2017-12-26 11:22:52 +0800

    Hello World ! :basketball:

--- app/src/main/java/com/example/zs/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/zs/kotlin/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.zs.kotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text_hello_world.text = "Hello World !"
     }
 }
---
+++ app/src/main/java/com/example/zs/kotlin/TestActivity.java
@@ -0,0 +1,17 @@
+package com.example.zs.kotlin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Description:
+ * Created by zs on 2017/12/26.
+ */
+
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
