8b03ef06c8f24017f2c1282a3195c55000721de7
张森 <christian_zs@163.com>
2017-12-27 14:50:41 +0800

    操作符重载 :basketball:

--- app/src/main/java/com/example/zs/kotlin/domain/model/DomainClasses.kt
+++ app/src/main/java/com/example/zs/kotlin/domain/model/DomainClasses.kt
@@ -4,6 +4,11 @@ package com.example.zs.kotlin.domain.model
  * Description: 实体类
  * Created by zs on 2017/12/27.
  */
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+//    operator fun get(position: Int): Forecast = dailyForecast[position]
+
+    fun size(): Int = dailyForecast.size
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
 
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
--- app/src/main/java/com/example/zs/kotlin/ui/adapter/ForecastListAdapter.kt
+++ app/src/main/java/com/example/zs/kotlin/ui/adapter/ForecastListAdapter.kt
@@ -16,14 +16,14 @@ class ForecastListAdapter(val datas: ForecastList) : RecyclerView.Adapter<Foreca
 
     }
 
-    override fun getItemCount(): Int = datas.dailyForecast.size
+    override fun getItemCount(): Int = datas.size()
 
     override fun onBindViewHolder(holder: ForecastListAdapter.ViewHolder?, position: Int) {
         // Kotlin 扩展函数with是一个非常有用的函数，它包含在Kotlin的标准库中。它接收一个对象和一个扩展函数作为
         // 它的参数，然后使这个对象扩展这个函数。这表示所有我们在括号中编写的代码都是作为对象（第一个参数）
         // 的一个扩展函数，我们可以就像作为this一样使用所有它的public方法和属性。
         // 当我们针对同一个对象做很多操作的时候这个非常有利于简化代码。
-        with(datas.dailyForecast[position]) {
+        with(datas[position]) {
             holder!!.textView.text = "$date - $description = $high / $low"
         }
     }
