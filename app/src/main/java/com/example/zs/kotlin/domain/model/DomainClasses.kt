e824e3a3691cc4cbe95229372d56a0c87436e6ee
张森 <christian_zs@163.com>
2017-12-27 17:10:36 +0800

    操作符重载 :basketball:

--- app/src/main/java/com/example/zs/kotlin/domain/model/DomainClasses.kt
+++ app/src/main/java/com/example/zs/kotlin/domain/model/DomainClasses.kt
@@ -5,8 +5,6 @@ package com.example.zs.kotlin.domain.model
  * Created by zs on 2017/12/27.
  */
 data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
-//    operator fun get(position: Int): Forecast = dailyForecast[position]
-
     fun size(): Int = dailyForecast.size
     operator fun get(position: Int): Forecast = dailyForecast[position]
 }
